db55b82b6ef9611c8799ded4e2c9f1406998a3f3
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2022-07-05 22:14:24 -0300

    objeto

---
+++ exercicios-js/objeto/oo.js
@@ -0,0 +1,25 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+  valor1,
+  valor2,
+  valor3,
+  processamento() {
+    //...
+  }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+// Princípios importantes:
+// 1. abstração - saber como utilizar o objeto e suas funções dentro dele, trazendo do mundo real e diminuindo sua complexidade para encaixar no software
+
+// 2. encapsulamento - é importante conhecer as mecanismos que eu preciso saber para utilizar determinado objeto, simplificar a interface para quem for manipulá-lo. Saber utilizar a divisão entre a interface do usuário e a parte interna do sistema.
+
+// 3. herança(prototype) - saber que os objetos podem herdar algumas características de outros lugares, criando uma relação de pai e filho.
+
+// 4. polimorfismo - a partir de uma variável mais genérica, conseguimos atribuir outras funções a essa variável pai.
